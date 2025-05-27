e162eaadac6be96016240a6ea010b6ac8923310e
Ujjwal Kumar Singh <ujjwalkumar0149@gmail.com>
2025-05-27 19:04:37 +0530

    Bugs Fixed

--- src/hooks/usePlatform.js
+++
@@ -1,6 +0,0 @@
-export default function usePlatform() {
-    const ua = navigator.userAgent || navigator.vendor || window.opera;
-    if (/android/i.test(ua)) return "android";
-    if (/iPad|iPhone|iPod/.test(ua) && !window.MSStream) return "ios";
-    return "desktop";
-  }
